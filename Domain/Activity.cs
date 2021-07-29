865a50b87961794fc553a20d8e26b3e81832931a
himanshu345-sys <mittalhimanshu38@gmail.com>
2021-07-29 17:28:28 +0530

    Error handling and exception done

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -5,8 +5,10 @@ namespace Domain
     public class Activity
     {
         public Guid Id { get; set; }
+
+      
         public string Title { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime? Date { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
         public string City { get; set; }
