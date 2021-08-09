4c6b3539fe4dc6dd86388c3ad564d91399258b09
himanshu345-sys <mittalhimanshu38@gmail.com>
2021-08-09 22:00:19 +0530

    Image Upload error solved

--- API/Startup.cs
+++ API/Startup.cs
@@ -55,7 +55,7 @@ namespace API
                 .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com","data:"))
                 .FormActions(s => s.Self())
                 .FrameAncestors(s => s.Self())
-                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com","blob:"))
                 .ScriptSources(s => s.Self().CustomSources("sha256-G0+hZwWbZJv9BVKtl7EuGzv5PzrgkOv27uDcI4yHKPs="))
             );
             
